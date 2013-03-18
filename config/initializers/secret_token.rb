942116426621810a6a799067ccdbb5be4bc87775
lalalainexd <lalalainexd@gmail.com>
2013-03-18 10:14:58 -0600

    Initial project generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContactManager::Application.config.secret_token = '0ec6ddf44a20eb343b2a596fa174940894bec13e68dcef4cd153bf3377cfeb1350c0be8ea806de57e1ce86378595a25e03c8134bf62518b07c0be34b1ab64e2a'
